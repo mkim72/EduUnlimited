a0c193366e7801098cf44b5ac4eb3ca9519517cf
Michelle Kim <makim7200@gmail.com>
2017-07-10 09:35:50 -0700

    AboutMe

---
+++ AboutMe/src/aboutme/AboutMe.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aboutme;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class AboutMe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is Michelle.");
+        System.out.println("I'm 17 years old.");
+        System.out.println("I'm from San Ramon.");
+        System.out.println("I enjoy reading, writing, and playing the piano.");
+        System.out.println("My favorite color is orange.");
+    }
+    
+}
