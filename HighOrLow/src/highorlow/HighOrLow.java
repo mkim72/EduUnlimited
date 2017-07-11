0dd719a575e3d9d33910ffb6eca6f52aa4d0fd1b
Michelle Kim <makim7200@gmail.com>
2017-07-11 10:14:52 -0700

    number guessing updates

--- HighOrLow/src/highorlow/HighOrLow.java
+++ HighOrLow/src/highorlow/HighOrLow.java
@@ -22,6 +22,7 @@ public class HighOrLow {
         int numberReal = (int)(Math.random()*100 + 1);
         int numberGuess = 0;
         int guesses = 0;
+        int hintValue = 0;
         System.out.println("I'm thinking of a number between 1 and 100!\n");
         while (numberReal != numberGuess) {
             System.out.print("What number am I thinking of? ");
@@ -38,6 +39,11 @@ public class HighOrLow {
                 guesses++;
                 System.out.println("\nCongratulations, you guessed it! I was thinking of " + numberReal + ".");
                 System.out.println("Only took you " + guesses + " guesses!");
+            }
+            //HINT :)
+            if (guesses == 10 && (numberReal != numberGuess)) {
+                hintValue = numberReal / 10 * 10;
+                System.out.println("Having trouble? Try numbers " + hintValue + " to " + (hintValue + 10) + ".\n");
             }
         }
     }
