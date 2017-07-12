2224aee2f0f90151a2a816e38145a797c0867311
Michelle Kim <makim7200@gmail.com>
2017-07-12 11:16:05 -0700

    puzzles add

--- MathIO2/src/mathio2/MathIO2.java
+++ MathIO2/src/mathio2/MathIO2.java
@@ -9,8 +9,8 @@ package mathio2;
  *
  * @author Education Unlimited
  */
-
 import java.io.*;
+
 public class MathIO2 {
 
     /**
@@ -18,7 +18,7 @@ public class MathIO2 {
      */
     public static void main(String[] args) {
         String FILENAME = "numbers.txt";
-        
+
         //Part 1
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))) {
             //here is where you should put your numbers
@@ -27,31 +27,29 @@ public class MathIO2 {
             //here is where you should write your numbers to the file
             writer.write(line);
             System.out.println("Done\n");
-
-	} 
+        } 
+        
         catch (IOException e) {
             e.printStackTrace();
+        }
 
-	}
-        
         //Part 2
         try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
 
             String currentLine = reader.readLine();
             double sum = 0.0;
-            
+
             while (currentLine != null) {
-		System.out.println("number: " + currentLine);
+                System.out.println("number: " + currentLine);
                 double number = Double.parseDouble(currentLine);
                 sum = sum + number;
                 System.out.println("sum: " + sum + "\n");
                 currentLine = reader.readLine(); // sets the while loop to read the next line
             }
-
-	}
+        } 
+        
         catch (IOException e) {
             e.printStackTrace();
-	}
+        }
     }
-    
 }
---
+++ Puzzles/src/puzzles/Puzzles.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puzzles;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Puzzles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String FILENAME = "puzzle.txt";
+	try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
+
+            String currentLine = reader.readLine();
+            int lineNumber = 1;
+
+            while (currentLine != null) {
+                if (lineNumber%3==0) {
+                    System.out.println(currentLine);
+                }
+                currentLine = reader.readLine();
+                lineNumber++;
+            }
+	}
+        catch (IOException e) {
+            e.printStackTrace();
+	}
+    }   
+}
